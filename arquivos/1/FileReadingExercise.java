b9b9a1fa991699b48f83eb794722a7577ece1c6c
Adilson Vicente dos Santos <adilson_mumu@hotmail.com>
2025-07-22 16:23:47 -0300

    file handling

---
+++ arquivos/1/FileReadingExercise.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+public class FileReadingExercise {
+    public static void main(String[] args) {
+        String fileName = "exemplo.txt"; // Nome do arquivo fixo para leitura
+
+        try ( BufferedReader buffRead = new BufferedReader(new FileReader(fileName));) {
+            String linha = "";
+            while (true) {
+                if (linha != null) {
+                    System.out.println(linha);
+
+                } else
+                    break;
+                linha = buffRead.readLine();
+            }
+        } catch (IOException e) {
+            System.out.println("Erro ao ler o arquivo" + e.getMessage());
+        }
+
+    }
+
+}
