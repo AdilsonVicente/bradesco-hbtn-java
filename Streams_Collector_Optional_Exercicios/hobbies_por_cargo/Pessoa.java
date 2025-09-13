8db7aec2727f4ce31cfdfe72409a766185691690
Adilson Vicente dos Santos <adilson_mumu@hotmail.com>
2025-09-12 23:52:06 -0300

    ajuste pessoa

--- Streams_Collector_Optional_Exercicios/hobbies_por_cargo/Pessoa.java
+++ Streams_Collector_Optional_Exercicios/hobbies_por_cargo/Pessoa.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 public class Pessoa implements Comparable<Pessoa> {
 
     private int codigo;
@@ -5,13 +7,15 @@ public class Pessoa implements Comparable<Pessoa> {
     private String cargo;
     private int idade;
     private double salario;
+    private List<String> hobbies;
 
-    public Pessoa(int codigo, String nome, String cargo, int idade, double salario) {
+    public Pessoa(int codigo, String nome, String cargo, int idade, double salario, List<String> hobbies) {
         this.codigo = codigo;
         this.nome = nome;
         this.cargo = cargo;
         this.idade = idade;
         this.salario = salario;
+        this.hobbies = hobbies;
     }
 
     public int getCodigo() {
@@ -54,6 +58,15 @@ public class Pessoa implements Comparable<Pessoa> {
         this.salario = salario;
     }
 
+
+    public List<String> getHobbies() {
+        return hobbies;
+    }
+
+    public void setHobbies(List<String> hobbies) {
+        this.hobbies = hobbies;
+    }
+
     @Override
     public int compareTo(Pessoa arg0) {
         return this.nome.compareTo(arg0.nome);
@@ -64,5 +77,4 @@ public class Pessoa implements Comparable<Pessoa> {
         return "[" + codigo + "] " + nome + " " + cargo + " " + idade + " R$ "
                 + String.format("%.6f", salario).replace(".", ",");
     }
-
 }
