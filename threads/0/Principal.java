3f5580792cdbc15eda2b6bbdef03b2a71daa567d
Adilson Vicente dos Santos <adilson_mumu@hotmail.com>
2025-09-18 10:57:06 -0300

    Threads

---
+++ threads/0/Principal.java
@@ -0,0 +1,20 @@
+
+public class Principal {
+    public static void main(String[] args) throws InterruptedException {
+
+        Contador contador = new Contador();
+
+        ThreadContador threads = new ThreadContador(contador);
+        threads.start();
+
+        ThreadContador threads1 = new ThreadContador(contador);
+        threads1.start();
+
+        threads.join();
+        threads1.join();
+
+        System.out.println("Valor final do contador: " + contador.getContagem());
+
+    }
+
+}
---
+++ threads/2/Consumidor.java
@@ -0,0 +1,19 @@
+public class Consumidor extends Thread {
+
+    public Fila fila;
+
+    public Consumidor(Fila fila) {
+        this.fila = fila;
+    }
+
+    @Override
+    public void run() {
+        try {
+            fila.retirar();
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
